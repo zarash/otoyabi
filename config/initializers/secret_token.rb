6f11e748577f6e213d0e46cb49ff07c56bf5144e
zarash <z.ashrafi@yahoo.com>
2013-12-17 00:06:34 +0330

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bama::Application.config.secret_token = 'a60eaddcc0398286e3ecfc8b483423d45e28e4c3e0593d778ab656481b84e2e34af87749d689c17530d3acc9c4aaca063ba467c2187af95b9401ad930a4cf8fd'
